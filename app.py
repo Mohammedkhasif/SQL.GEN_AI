68f8c7aa810baa8ce9721285a9a5fa13e0194e76
Mohammedkhasif <mohammedkhasif1@gmail.com>
2025-07-20 00:56:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="YOUR_API_KEY")  # Replace with your actual API key
+genai.configure(api_key="YOUR_API_KEY")  
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
